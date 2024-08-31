a5fa5ebf9c3ed6a4714da60233d41203e0f18f41
boyoanu <boyoanu@hotmail.com>
2024-08-31 18:11:02 +0100

    Updated cypress/e2e/LoginPage/LoginTest.js

--- cypress/e2e/LoginPage/LoginTest.js
+++ cypress/e2e/LoginPage/LoginTest.js
@@ -1,29 +1,28 @@
 beforeEach(()=>{
-    cy.visit('/loginPage');
-  
+  cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+
+})
+
+describe(' All Login Test', () => {
+    it('Successful login with valid ', () => {
+      cy.get('input[name="username"]').type('Admin')
+      cy.get('input[name="password"]').type('admin123')
+      cy.get('.oxd-button').click()
+    })
+
+    it('Failed login with invalid credentials', () => {
+      cy.get('input[name="username"]').type('Admin')
+      cy.get('input[name="password"]').type('admin1234')
+      cy.get('.oxd-button').click()
+      cy.get('.oxd-alert-content > .oxd-text').should('be.visible')
+    })
+
+    it('Displays error message on failed login', () => {
+      cy.get('input[name="username"]').type('Adminn')
+      cy.get('input[name="password"]').type('admin1234')
+      cy.get('.oxd-button').click()
+      cy.get('.oxd-alert-content > .oxd-text').should('contain', 'Invalid credentials')
+    })
+
   })
   
-  describe(' All Login Test', () => {
-      it('Successful login with valid credentials', () => {
-        cy.get('#username').type('validUsername')
-        cy.get('#password').type('validPassword')
-        cy.get('#loginButton').click()
-        cy.url().should('include', '/dashboard');  // Assuming a redirect to /dashboard
-      })
-  
-      it('Failed login with invalid credentials', () => {
-        cy.get('#username').type('invalidUsername')
-        cy.get('#password').type('invalidPassword')
-        cy.get('#loginButton').click();
-        cy.get('#errorMessage').should('be.visible')
-      })
-  
-      it('Displays error message on failed login', () => {
-        cy.get('#username').type('invalidUsername')
-        cy.get('#password').type('invalidPassword')
-        cy.get('#loginButton').click();
-        cy.get('#errorMessage').should('contain', 'Invalid credentials')
-      })
-  
-    })
-    
