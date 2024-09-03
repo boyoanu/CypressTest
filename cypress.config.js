d5e13efa676b67b363c7df92ecfaf1cb84ddc7ab
boyoanu <boyoanu@hotmail.com>
2024-09-03 18:38:29 +0100

    update cypress.config.js file

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    video : true,
     //baseUrl:'http://localhost:63352',
     excludeSpecPattern:['**/1-getting-started','**/2-advanced-examples'],
     specPattern:'cypress/e2e/**/*.{js,jsx,ts,tsx}',
